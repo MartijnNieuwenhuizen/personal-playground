41e630a0fe89ad076b46bedd037512250eadb368
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2023-10-18 21:50:47 +0200

    Update tour the france with nicer looks!

--- src/routes/playground/tour-the-france-stage/+page.svelte
+++ src/routes/playground/tour-the-france-stage/+page.svelte
@@ -18,12 +18,14 @@
 				<div class="stage-scroller" />
 			</div>
 
-			<div class="runner" />
+			<div class="runner"><span>🚴🏻</span></div>
 		</div>
 	</div>
 </PlaygroundItem>
 
-<style>
+scss
+
+<style type="scss">
 	:global(html) {
 		/* @TODO: Make this scoped to a component! */
 		scroll-timeline: --stage-timeline;
@@ -36,12 +38,46 @@
 	.sticky {
 		position: sticky;
 		top: 30%;
+
+		/* Create container so we can use container-relative units */
+		container-type: size;
+		aspect-ratio: 4 / 1;
 	}
 
-	/* img {
-		width: 100%;
-		height: auto;
-	} */
+	$stage-dots: (
+		(0, 63),
+		(3, 48),
+		(12, 76),
+		(15, 72),
+		(20, 81),
+		(21, 73),
+		(25, 72),
+		(27, 84),
+		(35, 80),
+		(44, 58),
+		(46, 63),
+		(50, 52),
+		(59, 75),
+		(65, 75),
+		(69, 47),
+		(75, 74),
+		(82, 28),
+		(85, 47),
+		(90, 15),
+		(98, 51),
+		(100, 51)
+	);
+
+	// Create a new list with percentages.
+	// Outcome: 0%, 63%, 3%, 48%, 12%, 76% etc.
+	$test: ();
+	@for $i from 1 through length($stage-dots) {
+		$item: nth($stage-dots, $i);
+		$test: append($test, calc(#{nth($item, 1)} * 1%) calc(#{nth($item, 2)} * 1%), comma);
+	}
+	// Append the two missing shapes for the clip-path (bottom-right and bottom-left).
+	$test: append($test, 100% 100%, comma);
+	$test: append($test, 0% 100%, comma);
 
 	.stage-outline {
 		width: 100%;
@@ -50,34 +86,7 @@
 		opacity: 0.5;
 		aspect-ratio: 4 / 1;
 
-		/* Full version */
-		clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
-		/* Stage outline */
-		clip-path: polygon(
-			0% 63%,
-			3% 48%,
-			12% 76%,
-			15% 72%,
-			20% 81%,
-			21% 73%,
-			25% 72%,
-			27% 84%,
-			35% 80%,
-			44% 58%,
-			46% 63%,
-			50% 52%,
-			59% 75%,
-			65% 75%,
-			69% 47%,
-			75% 74%,
-			82% 28%,
-			85% 47%,
-			90% 15%,
-			98% 51%,
-			100% 51%,
-			100% 100%,
-			0% 101%
-		);
+		clip-path: polygon($test);
 	}
 
 	@keyframes stage-scroller {
@@ -100,104 +109,88 @@
 	}
 
 	@keyframes runner-path {
-		0% {
-			left: 0%;
-			top: 63%;
-		}
-		3% {
-			left: 3%;
-			top: 48%;
-		}
-		12% {
-			left: 12%;
-			top: 76%;
-		}
-		15% {
-			left: 15%;
-			top: 72%;
-		}
-		20% {
-			left: 20%;
-			top: 81%;
-		}
-		21% {
-			left: 21%;
-			top: 73%;
-		}
-		25% {
-			left: 25%;
-			top: 72%;
-		}
-		27% {
-			left: 27%;
-			top: 84%;
-		}
-		35% {
-			left: 35%;
-			top: 80%;
-		}
-		44% {
-			left: 44%;
-			top: 58%;
-		}
-		46% {
-			left: 46%;
-			top: 63%;
-		}
-		50% {
-			left: 50%;
-			top: 52%;
-		}
-		59% {
-			left: 59%;
-			top: 75%;
-		}
-		65% {
-			left: 65%;
-			top: 75%;
-		}
-		69% {
-			left: 69%;
-			top: 47%;
-		}
-		75% {
-			left: 75%;
-			top: 74%;
-		}
-		82% {
-			left: 82%;
-			top: 28%;
-		}
-		85% {
-			left: 85%;
-			top: 47%;
-		}
-		90% {
-			left: 90%;
-			top: 15%;
-		}
-		98% {
-			left: 98%;
-			top: 51%;
-		}
-		100% {
-			left: 100%;
-			top: 51%;
+		@for $i from 1 through length($stage-dots) {
+			$item: nth($stage-dots, $i);
+
+			// Prevent getting the item after the last item.
+			$nextItemIndex: $i + 1;
+			@if $nextItemIndex > length($stage-dots) {
+				$nextItemIndex: $i;
+			}
+			$nextItem: nth($stage-dots, $nextItemIndex);
+
+			// Correct calculation, but wrong result.
+			// We want to use the first value in the array (for a reason I cannot understand yet).
+			// $keyframePercentage: round((($i / length($stage-dots)) * 100%));
+
+			// Calculate the percentage of the keyframe.
+			$keyframePercentage: (nth($item, 1) * 1%);
+			$nextKeyframePercentage: (nth($nextItem, 1) * 1% - 0.01%);
+
+			// Check if the cyclist is going up-hil, downhill or flat.
+			$orientation: 'flat';
+			$rotate: 0deg;
+			@if $i < length($stage-dots) {
+				@if nth($nextItem, 2) > nth($item, 2) {
+					$orientation: 'uphill';
+					$rotate: 45deg;
+				}
+				@if nth($nextItem, 2) < nth($item, 2) {
+					$orientation: 'downhill';
+					$rotate: -45deg;
+				}
+			}
+
+			// Create as many keyframes as there are items in the list.
+			#{$keyframePercentage} {
+				transform-origin: center;
+				transform: translateX(calc(#{nth($item, 1)}cqw - 50%))
+					translateY(calc(#{nth($item, 2)}cqh - 50%))
+					rotate($rotate);
+			}
+			// Create a next keyframe that's almost the same as the next item in the loop.
+			// But the orientation is the one of the current item in the loop.
+			// This ensures the cyclist is keeping the correct rotation throughout the entire part of the animation.
+			#{$nextKeyframePercentage} {
+				transform-origin: center;
+				transform: translateX(calc(#{nth($nextItem, 1)}cqw - 50%))
+					translateY(calc(#{nth($nextItem, 2)}cqh - 50%))
+					rotate($rotate);
+			}
 		}
 	}
+
+	/* 
+		@TODO: Use prettier colors.
+		@TODO: Write how this works on the page.
+		@TODO: Make even more dynamic so I can add multiple stages on this page! :).
+		@TODO: Check if I can improve this with https://developer.mozilla.org/en-US/docs/Web/CSS/offset-path !!!!!
+	*/
 
 	.runner {
 		position: absolute;
 		top: 0;
 		left: 0;
 
-		width: 1rem;
-		height: 0.65rem;
-		background-color: red;
-		transform: translate(-50%, -50%);
+		// width: 1rem;
+		// height: 0.65rem;
+		// background-color: red;
+		/* transform: translate(-50%, -50%); */
 
 		transform-origin: left;
 		animation: runner-path auto linear;
 		animation-timeline: --stage-timeline;
+		font-size: 1.5rem;
+
+		// transform: translateX(0cqw) translateY(calc(100cqh - 63cqh));
+		// transform-origin: top left;
+		// transform: translateX(0cqw) translateY(63cqb);
+
+		// transform: translateX(calc(0cqw - 50%)) translateY(calc(63cqh - 50%));
+	}
+
+	.runner span {
+		display: block;
+		transform: rotateY(180deg);
 	}
 </style>
