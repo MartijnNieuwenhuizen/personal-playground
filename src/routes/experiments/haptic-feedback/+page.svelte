c160bbcc251174908fe46e8e1f42d05ff1c86e26
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2024-02-26 16:51:06 +0100

    Update content

--- src/routes/experiments/haptic-feedback/+page.svelte
+++ src/routes/experiments/haptic-feedback/+page.svelte
@@ -1,13 +1,15 @@
 <script>
 	import ExperimentItem from '$lib/components/ExperimentItem/index.svelte';
-	import { onMount } from 'svelte';
 
 	const title = 'Haptic feedback';
 	const description = 'Create haptic feedback.';
 
 	const handleClick = () => {
-		navigator.vibrate(1000);
+		navigator.vibrate([200, 100, 200]);
+		console.log('navigator: ', navigator);
 	};
+
+	const downloadAsWebApp = () => {};
 </script>
 
 <ExperimentItem {title} {description} showPageEffect={true}>
@@ -23,6 +25,27 @@
 
 	<h3>Experiment 2 - Haptic feedback on click</h3>
 	<button on:click={handleClick}>Click me for some Haptic's</button>
+
+	<p>This doesn't work, also not on a mobile phone. I don't get any error's though.</p>
+
+	<h3>Experiment 3 - Haptic feedback with a WebApp</h3>
+	<p>
+		Maybe, it only works within a WebApp (progressive web app). It would make sense, a WebApp is
+		more "App-like" then a regular website. And the user needs to specifically download it before
+		you can use it.
+		<br />
+		That way, the user isn't going bonkers from Haptic Feedback from every website they visit. You could
+		go nuts with this as a Developer.
+		<br />
+		Let's give it a go
+	</p>
+
+	<h3>Conclusion</h3>
+	<p>
+		It's not a Progressive Web App feature, it's simply not supported on iOS and OSX. Therefore, I
+		cannot test this feature myself.
+	</p>
+	<button on:click={downloadAsWebApp}>Download this site as WebApp</button>
 </ExperimentItem>
 
 <style lang="scss">
