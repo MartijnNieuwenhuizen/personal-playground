06f3f4278d146a6f15d85a450f8f3df142e919d8
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2024-04-18 08:44:52 +0200

    Add widgets (but keep in comments for now)

--- src/routes/experiments/when-is-the-next-sunny-moment/+page.svelte
+++ src/routes/experiments/when-is-the-next-sunny-moment/+page.svelte
@@ -19,6 +19,8 @@
 				minute: 'numeric'
 		  })
 		: null;
+
+	// const weerOnlineIdBergen = 4058244;
 </script>
 
 <ExperimentItem {title} {description} showPageEffect={false}>
@@ -42,6 +44,44 @@
 			<figcaption>Conditie: {firstSunnyMoment.weather[0].description}</figcaption>
 		</figure>
 	{/if}
+
+	<!-- <div class="widgets">
+		<div style="max-width:500px" class="widget">
+			<iframe
+				title="widget"
+				width="100%"
+				height="433px"
+				frameBorder="0"
+				scrolling="no"
+				loading="lazy"
+				src={`https://www.weeronline.nl/widget/sun?id=${weerOnlineIdBergen}`}
+			/>
+		</div>
+
+		<div style="max-width:500px" class="widget">
+			<iframe
+				title="widget"
+				width="100%"
+				height="342px"
+				frameBorder="0"
+				scrolling="no"
+				loading="lazy"
+				src={`https://www.weeronline.nl/widget/activity?id=${weerOnlineIdBergen}&activity=running`}
+			/>
+		</div>
+
+		<div style="max-width:500px" class="widget">
+			<iframe
+				title="widget"
+				width="100%"
+				height="342px"
+				frameBorder="0"
+				scrolling="no"
+				loading="lazy"
+				src={`https://www.weeronline.nl/widget/activity?id=${weerOnlineIdBergen}&activity=bicycle`}
+			/>
+		</div>
+	</div> -->
 </ExperimentItem>
 
 <style lang="scss">
@@ -65,4 +105,14 @@
 	p {
 		font-size: 2rem;
 	}
+
+	// .widgets {
+	// 	display: grid;
+	// 	gap: 4rem 1rem;
+	// 	grid-template-columns: repeat(2, 1fr);
+	// }
+	// .widget:first-of-type {
+	// 	max-width: 100% !important;
+	// 	grid-column: 1 / -1;
+	// }
 </style>
